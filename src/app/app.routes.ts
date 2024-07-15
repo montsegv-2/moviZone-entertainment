cabb41ab38355f905fe63f3d4dc7d5403545f45e
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-07-15 06:11:00 -0600

    Fixing routing file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'admin',
     loadChildren: () =>
       import('./admin/admin.routes').then((m) => m.ADMIN_ROUTES),
   },
