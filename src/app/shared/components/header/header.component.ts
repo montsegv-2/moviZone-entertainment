62507b7c53bcc7cf7fc3a944651afb8563c82a2b
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-07-16 12:11:51 -0600

    Adding search bar in header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,18 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isSearchVisible = false;
+
+  toggleSearch() {
+    this.isSearchVisible = !this.isSearchVisible;
+  }
+}
